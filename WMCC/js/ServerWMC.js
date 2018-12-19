a5255d15feb5fa8a7df33cd121e7e60a07209150
ETdoFresh <ETdoFresh@gmail.com>
2018-12-19 14:07:08 -0600

    Add ServerWMC.js and drawings.svgz.

---
+++ WMCC/js/ServerWMC.js
@@ -0,0 +1,23 @@
+﻿var ServerWMC =
+{
+    /** @type {WebSocket} **/ Connection = undefined,
+    /** @type {string} **/ IPAddress = undefined,
+    /** @type {int} **/ Port = undefined,
+
+    GetConnection: function () { /** @returns {WebSocket} **/
+        if (this.Connection == undefined) {
+            this.Connection = new WebSocket('ws://' + this.IPAddress + ':' + this.Port);
+            this.Connection.onmessage = this.OnReceive;
+        }
+        else
+            return this.Connection;
+    },
+
+    Send: function (data) {
+        this.GetConnection().send(data);
+    },
+
+    OnReceive: function (e) {
+        console.log(e.data);
+    }
+};
