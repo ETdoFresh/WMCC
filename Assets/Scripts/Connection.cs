fb75ec220097c92d764409b79ae03ba1a6f55d62
ETdoFresh <etdofresh@gmail.com>
2019-01-30 12:32:57 -0600

    Add Connection class concept in Unity

---
+++ Assets/Scripts/Connection.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using UnityEngine;
+
+public class Connection : MonoBehaviour
+{
+    public ConnectionType connection;
+
+    private void OnEnable()
+    {
+        if (connection == null) return;
+        connection.Connect(OnConnect);
+    }
+
+    private void OnDisable()
+    {
+        if (connection == null) return;
+        connection.Disconnect();
+    }
+
+    private void OnConnect()
+    {
+        if (connection.IsConnected)
+            connection.Send("SomeCommand", OnReceive);
+    }
+
+    private void OnReceive()
+    {
+        ProcessCommand(connection.Data);
+    }
+
+    private void ProcessCommand(object data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public class ConnectionType
+    {
+        public bool IsConnected { get; internal set; }
+        public object Data { get; internal set; }
+
+        internal void Connect(Action onConnect)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Send(string v, Action onReceive)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [Serializable]
+    public class ServerWMCConnection : ConnectionType
+    {
+
+    }
+
+    [Serializable]
+    public class ServerDemoConnection : ConnectionType
+    {
+
+    }
+}
